1fcf37b23f2c6efd2dd7fa47adec0a5e75c2f399
changdigx <changdix.gui@intel.com>
2019-03-01 13:54:30 +0800

    Add Queue

---
+++ Queue.cpp
@@ -0,0 +1,28 @@
+#include "Queue.h"
+#include <iostream>
+
+#define MALLOC(S) (S)malloc(sizeof(S))
+int
+IsEmpty(Queue Q)
+{
+	return Q->Size == 0;
+}
+
+int
+IsFull(Queue Q)
+{
+	return Q->Size == MaxQueueSize;
+}
+
+Queue CreateQueue(int num)
+{
+	Queue S;
+	if (num<MinQueueSize || num>MaxQueueSize)
+		std::cout << "Queue size is illegal" << std::endl;
+	S = MALLOC(Queue);
+	S->Array = (int*)malloc(sizeof(Queue)*num);
+	if (S->Array == NULL)
+		std::cout << "Out of space" << std::endl;
+	S->Capacity = num;
+	return S;
+}
