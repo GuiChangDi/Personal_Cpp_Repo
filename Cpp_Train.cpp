0f63f9d05f306f1ab28dab25125878e9703d3034
changdigx <changdix.gui@intel.com>
2019-03-07 16:25:48 +0800

    Add Tree and Cpp Train

---
+++ Cpp_Train.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int plus(int x)
+{
+	x++;
+	return x;
+}
+
+/*
+int main()
+{
+	int a = 1;
+	int b;
+	b = plus(a);
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cin.get();
+	return 0;
+}
+
+*/
