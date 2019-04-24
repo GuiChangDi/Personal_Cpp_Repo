76b436ee873b3cc3881c2285552cd3782d22de37
changdigx <changdix.gui@intel.com>
2019-04-24 14:40:08 +0800

    Add Stack_class declare

---
+++ Header/Stack_class.h
@@ -0,0 +1,19 @@
+//Using Cpp class to define stack
+#pragma once
+typedef unsigned long Item;
+
+class Stack
+{
+private:
+    enum {MAX = 10};
+    Item items[MAX];
+    int top;//index for stack position
+public:
+    Stack();
+    bool isempty() const;
+    bool isfull() const;
+    // push() return false if stack already full, true otherwise.
+    bool push(const Item & item);
+    // pop() return false if stack already empty, true otherwise.
+    bool pop(const Item & item);
+}
