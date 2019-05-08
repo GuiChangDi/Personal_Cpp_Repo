2b5e87ca103a04315c26975f1d892eca5a5da3e9
changdig <guichangdi@gmail.com>
2019-05-08 23:23:08 +0800

    Add Brass/Brass Plus class

--- Header/OOP00.h
+++ Header/OOP00.h
@@ -67,3 +67,34 @@ public:
     unsigned int Rating() const { return rating;};
     void ResetRating ( unsigned int r) {rating = r;};
 };
+
+class Brass
+{
+private:
+    string Name;
+    long accNum;
+    double balance;
+public:
+    Brass(const string & n = "None",long an = -1,double ba = 0.0);
+    void Deposit(double n);
+    virtual void Withdraw(double n);
+    double Balance() const;
+    virtual void ViewAcct() const;
+    virtual ~Brass() {};
+};
+
+class BrassPlus: public Brass
+{
+private:
+    double maxLoan;
+    double rate;
+    double owesBank;
+public:
+    BrassPlus(const string & n = "None", long an = -1,double bal = 0.0, double ml = 500,double r = 0.11125);
+    BrassPlus(const Brass & ba, double ml = 500,double r = 0.11125);
+    virtual void ViewAcct() const;
+    virtual void Withdraw(double amt);
+    void ResetMax(double m) {maxLoan = m;};
+    void ResetRate(double r) {rate = r;};
+    void ResetOwes() { owesBank = 0;};
+};
