dd52654b3f16e2c4addcba4a2290180e11e58446
changdig <guichangdi@gmail.com>
2019-05-09 21:54:09 +0800

    Add vector

---
+++ Cpp/STL.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+
+int main()
+{
+    int a[6] = {1,2,3,4,5,6};
+    vector<int> b;
+    for(int i = 1;i<=4;i++)
+        b.push_back(a[i])
+
+}
