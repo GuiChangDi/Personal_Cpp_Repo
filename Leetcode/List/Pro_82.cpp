5345a302c77fa1708ed6b226cb4a9c4c55fe5aee
changdig <guichangdi@gmail.com>
2019-05-15 20:54:27 +0800

    Add Pro_82

---
+++ Leetcode/List/Pro_82.cpp
@@ -0,0 +1,33 @@
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ *  leaving only distinct numbers from the original list.
+ * */
+#define NULL 0
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    struct ListNode *p,*q,*temp;
+    p = head;
+    temp = p;
+    q = head->next;
+    while(p->next != NULL)
+    {
+        if(p ->val == q->val)
+        {
+            temp = p->next;
+            p->next = q->next;
+            free(temp);
+            q = p->next;
+        }
+        else
+        {
+            p = p->next;
+            q = q->next;
+        }
+    }
+    return head;
+}
