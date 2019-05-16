aea528c3da5bfd6b4262433b363af04ccfa8a458
changdigx <changdix.gui@intel.com>
2019-05-16 10:21:26 +0800

    Add STL method

--- Cpp/STL.cpp
+++ Cpp/STL.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <string>
 #include <vector>
-using std::vector;
+const int NUM = 5;
 
 int main()
 {
-    int a[6] = {1,2,3,4,5,6};
-    vector<int> b;
-    for(int i = 1;i<=4;i++)
-        b.push_back(a[i])
-
+    using std::vector;
+    using std::string;
+    using std::cin;
+    using std::cout;
+    vector<int> ratings(NUM);
+    vector<string> titles(NUM);
+    cout<<"You will do exactly as told. You will enter\n"
+        <<NUM<<" book titles and your ratings (0-10).\n";
+    int i;
+    for(int i = 0; i< NUM;i++)
+    {
+        cout<<" Enter title #"<< i+1 << ": ";
+        getline(cin,titles[i]);
+        cout<<" Enter your rating (0-10): ";
+        cin>>ratings[i];
+        cin.get();
+    }
+    cout<<"Thank you. You entered the following:\n"
+        << "Rating\tBook\n";
+    for(i = 0;i<NUM;i++)
+    {
+        cout<<ratings[i]<<"\t"<<titles[i]<<std::endl;
+    }
+    return 0;
 }
