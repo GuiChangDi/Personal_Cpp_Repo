738ca519667f91b47c6900361d4417645667fd94
chandig <changdix.gui@intel.com>
2019-09-20 16:50:21 +0800

    Add insertionSort selectMax selectionSort merge algorithm

--- Data_structure/Cpp_list.cpp
+++ Data_structure/Cpp_list.cpp
@@ -176,3 +176,64 @@ List<T>::sort(ListNodePosi(T) p, int n){
     default:  mergeSort(p, n); break;
   }
 }
+
+template <typename T>
+void
+List<T>::insertionSort(ListNodePosi(T) p, int n){
+  for (int r = 0; r < n; r++){
+    insertAfter(search(p->data, r ,p), p->data);
+    p = p->succ;
+    remove(p->pred);
+  }
+}
+
+template <typename T>
+ListNodePosi(T)
+List<T>::selectMax(ListNodePosi(T) p, int n){
+  ListNodePosi(T) max = p;
+  for (ListNodePosi9T) cur = p; 1 < n; n--)
+    if (!lt((cur = cur->succ)->data, max->data))
+      max = cur;
+  return max;
+}
+
+template <typename T>
+void
+List<T>::selectionSort(ListNodePosi(T) p, int n){
+  ListNodePosi(T) head = p->pred;
+  ListNodePosi(T) tail = p;
+  for (int i = 0; i < n; i++) tail = tail->succ;
+  while (1 < n){
+    ListNodePosi(T) max = selectMax(head->succ, n);
+    insertBefore(tail, remove(max));
+    tail = tail->pred;
+    n--;
+  }
+}
+
+template <typename T>
+void
+List<T>::merge(ListNodePosi(T) &p, int n, List<T> &L, ListNodePosi(T) q, int m){
+  //assert: this.valid(p) && rank(p) + n <= size && this.sorted(p,n)
+  //        L.valid(q) && rank(q) + m <= L._size && L.sorted(q,m)
+  ListNodePosi(T) pp = p->pred;
+  while (0 < m)
+    if ((0 < n) && (p->data <= q->data))
+      { if (q == (p = p->succ)) break; n-- }
+    else
+    {  insertBefore(p, L.remove((q = q->succ)->pred)); m--; }
+  p = pp->succ;
+}
+
+template <typename T>
+void
+List<T>::mergeSort(ListNodePosi(T) &p, int n){
+  if (n < 2) return;
+  int m = n >> 1;
+  ListNodePosi(T) q = p;
+  for (int i = 0;i < m; i+=)
+    q = q->succ;
+  mergeSort(p, m);
+  mergeSort(q, n - m);
+  merge(p, m, *this, q, n - m);
+}
