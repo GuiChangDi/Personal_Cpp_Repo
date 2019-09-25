0e5a0ec7884afef338ea1b4bd560dc99fbea0f10
chandig <changdix.gui@intel.com>
2019-09-25 21:02:36 +0800

    Add Tree relate Info

---
+++ Data_structure/Header/Cpp_tree.h
@@ -0,0 +1,18 @@
+/**
+Relate Words: for one node v
+ * vertex 顶点
+ * edge 边
+ * root 根
+ * noe 节点
+ * depth 深度
+ * ancestor 祖先
+ * descendant 后代
+ * parent 父亲
+ * child 孩子
+ * degree 度 deg(v)
+ * subtree 子树 subtree(v)
+ * height 高度
+ * proper ancestor 真祖先
+ * proper descendant 真后代
+ * proper binary tree 真二叉树 不含一度节点
+**/
