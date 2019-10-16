4209d0c7baa083e8b051f01a91db8581a99b3fff
chandig <changdix.gui@intel.com>
2019-10-16 14:30:41 +0800

    Add Huffman_Encode struct

--- Algorithm_example/Huffman_Encode.cpp
+++ Algorithm_example/Huffman_Encode.cpp
@@ -1,3 +1,17 @@
 #include "Header/Cpp_tree.h"
 #include <iostream>
 using namespace std;
+
+#define TreeNode(T) TreeNode<T>*
+
+template <typename T>
+struct TreeNode{
+  T data;
+  TreeNode(T) parent;
+  TreeNode(T) lChild;
+  TreeNode(T) rChild;
+  int height;
+  TreeNode(T a , TreeNode(T) p = NULL, TreeNode(T) lC = NULL, TreeNode(T) rC = NULL, int h = 0;)
+          : data(a), parent(p), lC(lChild), rC(rChild), h(height) {}
+};
+
