ff1e8c76a88a109df3414bb61c62c6c11e466e9f
changdig <changdig@foxmail.com>
2020-05-19 16:41:55 +0800

    Replace with one runable solution

--- Leetcode/List/Pro_02.cpp
+++ Leetcode/List/Pro_02.cpp
@@ -6,33 +6,35 @@ struct ListNode{
   ListNode(int x) : val(x), next(NULL) {}
 };
 
+/*
+Input example:
+  [2,4,3] -> 342
+  [5,6,4] -> 465
+Output should be:
+  [7,0,8] -> 807
+*/
+
 class Solution{
 public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
-    ListNode* l3 = new ListNode(0);
-    ListNode* p1 = l1;
-    ListNode* p2 = l2;
-    ListNode* p3 = l3;
-    bool flag = 0;
+    ListNode* l3 = NULL;
+    ListNode** p3 = &l3;
     int sum = 0;
-    while(p1 != NULL || p2 != NULL){
-      sum = 0;
-      if (p1->next != NULL){
-        sum += p1->val;
-        p1 = p1->next;
-      }
 
-      if (p2->next != NULL){
-        sum += p2->val;
-        p2 = p2->next;
+    while( l1 != NULL || l2 != NULL || sum > 0){
+      if ( l1 != NULL){
+        sum += l1->val;
+        l1 = l1->next;
       }
-
-      if(flag) sum++;
-      p3->next = new ListNode(sum%10);
-      p3 = p3->next;
-      flag = (sum>=10)? true : false;
+      if ( l2 != NULL){
+        sum += l2->val;
+        l2 = l2->next;
+      }
+      
+      *p3 = new ListNode(sum % 10);
+      sum /= 10;
+      p3 = &((*p3)->next);
     }
-    if(flag)  p3->next = new ListNode(1);
     return l3;
   }
 };
