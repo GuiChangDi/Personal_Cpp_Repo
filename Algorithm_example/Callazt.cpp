ee0f5da044a2cc887e1bec971e831ae46c513378
Changdi Gui <changdig@foxmail.com>
2020-06-07 20:48:27 +0800

    Add Calazt.cpp

---
+++ Algorithm_example/Callazt.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+//Callatz猜想实现 c version
+
+void Callatz(){
+    int step = 0, n;
+    scanf("%d", &n);
+    while( n != 1){
+        if (n%2 == 0) n /= 2;
+        if (n%2 == 1)
+            n = (3*n+1) / 2;
+        step ++;
+    }
+    printf("Step: %d\n", step);
+}
+
+int main(){
+    Callatz();
+    return 0;
+}
