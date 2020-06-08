b1a5cc3530df54b59fdff595cfe5fad246d02123
Changdi Gui <changdig@foxmail.com>
2020-06-08 15:12:09 +0800

    Add PATB1032.cpp

---
+++ PAT/PATB1032.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+const int MAXSIZE = 10;
+
+int main()
+{
+    int sum[MAXSIZE] = {0};
+    int k,j, MAX, n;
+    printf("Input n:");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        printf("Input the %d number and score:", i);
+        scanf("%d%d", &k, &j);
+        sum[k] += j;
+    }
+    MAX = 0;
+    for (int i = 1; i <= n; i++){
+        if (sum[MAX] <= sum[i])
+            MAX = i;
+    }
+    printf("Max score is: %d %d\n", MAX, sum[MAX]);
+    return 0;
+}
