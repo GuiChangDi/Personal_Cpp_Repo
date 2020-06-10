803670d940aded7a1cdebbaf4afc93b5b0b58243
Changdi Gui <changdig@foxmail.com>
2020-06-10 16:53:31 +0800

    Add PATB1036.cpp

---
+++ PAT/PATB1036.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int main()
+{
+    int n, l;
+    char s;
+    printf("Input n and character:");
+    scanf("%d %c", &n,&s);
+    if ((n%2) == 0) l = n/2;
+    else l = n/2 + 1;
+
+    for (int i = 0; i < n; i++)
+        printf("%c", s);
+    printf("\n");
+    for (int i = 0;i < l - 2; i++){
+        printf("%c", s);
+        for (int j = 0;j < n - 2; j++)
+            printf(" ");
+        printf("%c\n", s);
+    }
+    for (int i = 0; i < n; i++)
+        printf("%c", s);
+    return 0;
+}
