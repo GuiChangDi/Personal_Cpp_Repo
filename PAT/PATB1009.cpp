af6aa72652c7d1b38fd85d21ab6e16879dfcb67b
changdig <changdig@foxmail.com>
2020-06-13 18:20:28 +0800

    Add PATB1009.cpp

---
+++ PAT/PATB1009.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+	char str[90] = {0};
+	char s[90][90] = {0};
+	gets(str);
+    gets_s(str);//use gets_s on visual studio
+	int len = strlen(str);
+	int l = 0, h = 0;
+	for (int i = 0; i < len; i++) {
+		if (str[i] != ' ') {
+			s[l][h] = str[i];
+			h++;
+		}
+		else {
+			s[l][h] = '\0';
+			l++;
+			h = 0;
+		}
+	}
+	for (int i = l; i >= 0; i--) {
+		printf("%s", s[i]);
+		if(i > 0) printf(" ");
+	}
+	return 0;
+}
